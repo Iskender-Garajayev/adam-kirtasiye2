84b0457a33dffc423785946ae7bbb06019d23013
Iskender-Garajayev <yoloten.isa@gmail.com>
2023-11-28 10:44:53 -0800

    2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className='footer-container'>
+    <div className="footer-inner">
+      <p>ada'm kirtasiye</p>
+    </div>
+  </div>;
 };
 
 export default Footer;
